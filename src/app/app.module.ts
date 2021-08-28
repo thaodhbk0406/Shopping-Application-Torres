693d801f89ad655832937de031e0a91d1f149c5a
thao0406 <thaodhbk95@gmail.com>
2021-08-28 19:20:08 +0700

    First commit to compare code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropDownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { FormsModule } from '@angular/forms';
+import { RecipeService } from './recipes/recipe.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    DropDownDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,44 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'First Test Recipe',
+      'This is the first simple recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574',
+        [
+            new Ingredient('Meat', 6),
+            new Ingredient('Beef steak', 9)
+        ]
+      ),
+    new Recipe(
+      'Second Test Recipe',
+      'This is the second simple recipe',
+      'https://tse4.mm.bing.net/th?id=OIP.W8apTKARTqVhmLzMCisNmAHaEs&pid=Api&P=0&w=255&h=162',
+        [
+            new Ingredient('Meat oil', 6),
+            new Ingredient('Beef steak and butter', 9)
+        ]
+      ),
+  ];
+
+  constructor(private slService: ShoppingListService){
+
+  }
+
+  getRecipes() {
+    return this.recipes;
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
